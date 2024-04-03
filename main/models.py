c2b8b45772075dcd303d205186d3ee4fc201a2ff
Josef Vas <yovzhi@icloud.com>
2024-04-03 17:21:05 +0200

    1233

---
+++ main/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        pass
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    image = models.ImageField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='дата обновления')
+    manufactured_at = models.DateTimeField(auto_now_add=True, verbose_name='дата производства')
+
+    def __str__(self):
+        return (f'{self.name} {self.description} {self.price} {self.image} {self.category} {self.price}'
+                f'{self.created_at} {self.updated_at}')
+
+    class Meta:
+        pass
+
+
